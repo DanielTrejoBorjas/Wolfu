909b41c2774d876ac4b295783d579f3bf45c723f
DanielTrejoBorjas <danieltrejoborjas@gmail.com>
2025-06-17 21:36:04 -0600

    change wolfu::coreutils::command::Command to ::coreutils::command::Command in register_commands

--- src/macros/register_commands.rs
+++ src/macros/register_commands.rs
@@ -1,8 +1,7 @@
-// Register command in hashmap
 #[macro_export]
 macro_rules! register_commands {
     ( $( $name:literal => $cmd:expr ),* $(,)? ) => {{
-        let mut map: std::collections::HashMap<&'static str, Box<dyn wolfu::coreutils::command::Command>> = std::collections::HashMap::new();
+        let mut map: std::collections::HashMap<&'static str, Box<dyn $crate::coreutils::command::Command>> = std::collections::HashMap::new();
         $(
             map.insert($name, Box::new($cmd));
         )*
